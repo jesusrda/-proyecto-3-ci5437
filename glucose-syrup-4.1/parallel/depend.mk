bed3aebc10de9e9ea05054dc1a67b0987772b6e1
Jesus De Aguiar <jrdadb@gmail.com>
2021-07-21 02:06:57 -0400

    Eliminando archivos innecesarios

--- glucose-syrup-4.1/parallel/depend.mk
+++ glucose-syrup-4.1/parallel/depend.mk
@@ -147,3 +147,45 @@
  /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/parallel/../parallel/SolverCompanion.h \
  /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/parallel/../parallel/ClausesBuffer.h \
  /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/parallel/../parallel/SolverConfiguration.h
+/home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/Main.o /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/Main.or /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/Main.od /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/Main.op: \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/Main.cc \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../utils/System.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../mtl/IntTypes.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../utils/ParseUtils.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../utils/Options.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../mtl/Vec.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../mtl/XAlloc.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../core/Dimacs.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../core/SolverTypes.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../mtl/Alg.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../mtl/Map.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../mtl/Alloc.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../simp/SimpSolver.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../mtl/Queue.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../core/Solver.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../mtl/Heap.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../core/BoundedQueue.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../core/Constants.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../mtl/Clone.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../core/SolverStats.h
+/home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/SimpSolver.o /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/SimpSolver.or /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/SimpSolver.od /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/SimpSolver.op: \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/SimpSolver.cc \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../mtl/Sort.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../mtl/Vec.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../mtl/IntTypes.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../mtl/XAlloc.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../simp/SimpSolver.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../mtl/Queue.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../core/Solver.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../mtl/Heap.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../mtl/Alg.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../utils/Options.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../utils/ParseUtils.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../core/SolverTypes.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../mtl/Map.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../mtl/Alloc.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../core/BoundedQueue.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../core/Constants.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../mtl/Clone.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../core/SolverStats.h \
+ /home/jesusrda/Documents/USB/proyecto-3-ci5437/glucose-syrup-4.1/simp/../utils/System.h
